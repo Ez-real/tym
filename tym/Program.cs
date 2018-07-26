799acc7ae15251187e35f36fdafa6f3e610e900c
Ming <779715566@qq.com>
2018-07-26 09:15:05 +0800

    2tym

--- tym/Program.cs
+++ tym/Program.cs
@@ -11,7 +11,14 @@ namespace tym
         static void Main(string[] args)
         {
 
-
+            Console.WriteLine("tym");
+            string[] str = { "a", "b", "c", "d", "e", "f" };
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                string temp = str[i];
+                str[i] = str[str.Length - 1 - i];
+                str[str.Length - 1 - i] = temp;
+            }
         }
     }
 }
