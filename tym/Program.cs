fd748eaca5ca66d30ac0e23e1963272802896dfe
493028713 <493028713@qq.com>
2018-07-26 09:10:12 +0800

    1肖蕾

--- tym/Program.cs
+++ tym/Program.cs
@@ -19,6 +19,18 @@ namespace tym
                 str[i] = str[str.Length - 1 - i];
                 str[str.Length - 1 - i] = temp;
             }
+
+
+
+
+            string[] myArray = new string[3] { "世界杯", "欧洲杯", "欧冠" };//声明一个string数组并初始化
+            int i = 0;
+            do//调用do…while语句
+            {
+                Console.WriteLine(myArray[i]);//输出数组中数据
+                i++;
+            } while (i < myArray.Length);//设置do…while语句的条件
+            Console.ReadLine();
         }
     }
 }
